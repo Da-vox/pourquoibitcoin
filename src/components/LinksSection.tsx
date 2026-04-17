5ed8151df2122b99b306a7b5bb6a1370860001d0
Da-vox <poencindavy@hotmail.fr>
2026-04-17 20:41:04 +0200

    Update LinksSection.tsx

--- src/components/LinksSection.tsx
+++ src/components/LinksSection.tsx
@@ -23,7 +23,7 @@ const LINKS:
   "description": "Une vidéo claire et pédagogique de 12 minutes qui explique simplement le fonctionnement du minage de Bitcoin : l’énergie, le hardware, la difficulté, les récompenses et pourquoi c’est essentiel à la sécurité du réseau.",
   "image": "https://pbs.twimg.com/profile_images/1369025687743500288/-4tdBSu5.jpg",
   "date": "2026-04-17"
-}
+},
 
 
 {
